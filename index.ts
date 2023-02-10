81e4f8dd1b53ac4e19d14efda651e339b1690c95
Davide Luppoli <davide@davideluppoli.it>
2023-02-10 13:03:54 +0100

    OOP parte 2

--- index.ts
+++ index.ts
@@ -2,29 +2,61 @@ class Libro
 {
     //prop --> dati
     private readonly dataCreazione:Date;
-    //titolo:string;
-    //prezzo:number;
-    //editore:string = "Maggioli Editore";
+
+    //costruttori --> Funzionalità di setup
+    constructor(titolo:string, _prezzo:number);
+    constructor(titolo:string, _prezzo:number, editore:string);
+    constructor(public readonly titolo:string, private _prezzo:number, public readonly editore:string = "Maggioli Editore")
+    {
+        this.dataCreazione = new Date();
+    }
 
     //metodi --> Funzionalità
     getInfo() : string
     {
-        return `Titolo: ${this.titolo} - Prezzo ${this.prezzo} - Creato il:${this.dataCreazione}`;
+        return `Titolo: ${this.titolo} - Prezzo ${this._prezzo} - Creato il:${this.dataCreazione}`;
     }
 
-    //costruttori --> Funzionalità di setup
-    constructor(titolo:string, prezzo:number);
-    constructor(titolo:string, prezzo:number, editore:string);
-    constructor(public titolo:string, public prezzo:number, public editore:string = "Maggioli Editore")
+    get prezzo() : number { return this._prezzo; }
+    set prezzo(p:number) { this._prezzo = p<=0 ? 1000 : p }
+}
+
+
+class LibroCartaceo extends Libro
+{
+    constructor(titolo:string, prezzo:number, public pagine:number, editore:string ="Maggioli")
     {
-        this.dataCreazione = new Date();
-        //this.titolo = titolo;
-        //this.prezzo = prezzo;
-        //this.editore = editore;
+        super(titolo,prezzo,editore);
     }
 }
+
+
+
 
 let l1 : Libro = new Libro('I promessi sposi',10,'Mondadori');
 let l2 = new Libro('La divina commedia',12);
 
-console.log(l1.getInfo());
+
+let l3 : LibroCartaceo = new LibroCartaceo('Ritratto in seppia',14,123);
+
+
+
+
+
+
+/*
+let l3 : Libro = l1;
+l3.prezzo = 20;
+console.log(l1.prezzo);
+
+
+
+class MathUtils
+{
+    static readonly pi : number = 3.1415;
+}
+
+let raggio = 10;
+
+//let m = new MathUtils();
+let area = raggio*raggio*MathUtils.pi;*/
